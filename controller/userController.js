2fc243076e3ff7b06904fc104ee799840c0dabc0
Vladimir <22523298@students.uii.ac.id>
2024-06-08 14:16:41 +0700

    Check

--- controller/userController.js
+++ controller/userController.js
@@ -5,11 +5,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const register = async (req, res, pool) => {
-    const { username, email, password } = req.body;
+    const { name, email, password } = req.body;
     try{
         const hashedPassword = await bcrypt.hash(password, 10);
         await pool.query(
-            'INSERT INTO users (username, password) VALUES ($1, $2)',[username, hashedPassword]
+            'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)',[name, email, hashedPassword]
         );
         res.status(201).json({message: 'User Registered Successfully'});
       }catch(err){
--- server.js
+++ server.js
@@ -9,9 +9,17 @@ const app = express();
 app.use(express.json());
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
+app.use((req, res, next) => {
+    req.pool = pool;
+    next();
+})
 console.log('Connecting to PostgreSQL');
 
 pool.connect()
@@ -19,44 +27,33 @@ pool.connect()
     .catch(err => console.error('PostgreSQL error:', err));
 
 app.use((req, res, next) => {
-    req.pool = pool;
-    next();
-});
+        req.pool = pool;
+        next();
+    })
 
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes)
 
-app.use('/', (req, res) => {
-    res.json({ message: 'Hello, world!' });
-});
+const PORT = process.env.PORT;
+const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-const PORT = process.env.PORT || 3000;
-
-if (!module.parent) {
-    const server = app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`);
-    });
-
-    process.on('SIGTERM', () => {
-        console.log('SIGTERM signal received: closing HTTP server');
-        server.close(() => {
-            console.log('HTTP server closed');
-            pool.end(() => {
-                console.log('PostgreSQL pool has ended');
-            });
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server')
+    server.close(() => {
+        console.log('HTTP server closed')
+        pool.end(() => {
+            console.log('PostgreSQL pool has ended')
         });
     });
+});
 
-    process.on('SIGINT', () => {
-        console.log('SIGINT signal received: closing HTTP server');
-        server.close(() => {
-            console.log('HTTP server closed');
-            pool.end(() => {
-                console.log('PostgreSQL pool has ended');
-            });
+process.on('SIGINT', () => {
+    console.log('SIGINT signal received: closing HTTP server')
+    server.close(() => {
+        console.log('HTTP server closed')
+        pool.end(() => {
+            console.log('PostgreSQL pool has ended')
         });
     });
+});
 
-    module.exports = server;
-} else {
-    module.exports = app;
-}
+module.exports = server;
