cd6255e5fdbe960f6c11879efafeddb505214be7
Vladimir <22523298@students.uii.ac.id>
2024-06-08 15:04:25 +0700

    fix

--- server.js
+++ server.js
@@ -9,12 +9,8 @@ const app = express();
 app.use(express.json());
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-});
+    connectionString: process.env.POSTGRES_URL,
+  })
 
 app.use((req, res, next) => {
     req.pool = pool;
